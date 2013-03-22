ece71725f78ee2abfbdfdeddc4cc6fd269fb764f
Espen Moe-Nilssen <espen@medialog.no>
2013-03-22 09:46:12 +0100

    New footer, set from settings

--- medialog/nidelventheme/browser/interfaces.py
+++ medialog/nidelventheme/browser/interfaces.py
@@ -4,6 +4,5 @@ from plone.theme.interfaces import IDefaultPloneLayer
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
        If you need to register a viewlet only for the
-       "Kongsberg Theme" theme, this interface must be its layer
-       (in kongsbergtheme/viewlets/configure.zcml).
+       "Nidelven Theme" theme, this interface must be its layer
     """
